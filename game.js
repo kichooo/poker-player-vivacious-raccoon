026f68303465c67c309e8ead76cc57552b38e5fb
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 11:00:00 +0200

    reformat and game struct

---
+++ game.js
@@ -0,0 +1,33 @@
+module.exports = {
+    // The small blind in the current round. The big blind is twice
+    // the small blind
+    SmallBlind: int `json:"small_blind"`
+
+    // The amount of the largest current bet from any one player
+    CurrentBuyIn int `json:"current_buy_in"`
+
+    // The size of the pot (sum of the player bets)
+    Pot int `json:"pot"`
+
+    // Minimum raise amount. To raise you have to return at least:
+    //     current_buy_in - players[in_action][bet] + minimum_raise
+    MinimumRaise int `json:"minimum_raise"`
+
+    // The index of the player on the dealer button in this round
+    // The first player is (dealer+1)%(players.length)
+    Dealer int `json:"dealer"`
+
+    // Number of orbits completed. (The number of times the dealer button
+    // returned to the same player.)
+    Orbits int `json:"orbits"`
+
+    // The index of your player, in the players array
+    InAction int `json:"in_action"`
+
+    // An array of the players. The order stays the same during the
+    // entire tournament
+    Players []Player `json:"players"`
+
+    // Finally the array of community cards.
+    CommunityCards []Card `json:"community_cards"`
+}
--- player_service.js
+++ player_service.js
@@ -4,25 +4,25 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res){
-  res.send(200, 'OK')
+app.get('/', function (req, res) {
+    res.send(200, 'OK')
 });
 
-app.post('/', function(req, res){
-  if(req.body.action == 'bet_request') {
-    player.bet_request(JSON.parse(req.body.game_state),function(bet) {
-      res.send(200, bet.toString());
-    });
-  } else if(req.body.action == 'showdown') {
-    player.showdown(JSON.parse(req.body.game_state));
-    res.send(200, 'OK');
-  } else if(req.body.action == 'version') {
-    res.send(200, player.VERSION);
-  } else {
-    res.send(200, 'OK')
-  }
+app.post('/', function (req, res) {
+    if (req.body.action == 'bet_request') {
+        player.bet_request(JSON.parse(req.body.game_state), function (bet) {
+            res.send(200, bet.toString());
+        });
+    } else if (req.body.action == 'showdown') {
+        player.showdown(JSON.parse(req.body.game_state));
+        res.send(200, 'OK');
+    } else if (req.body.action == 'version') {
+        res.send(200, player.VERSION);
+    } else {
+        res.send(200, 'OK')
+    }
 
 });
 
