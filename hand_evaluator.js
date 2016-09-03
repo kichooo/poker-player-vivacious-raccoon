2a6aae9bdb22e9e09815a2a09f503dcf1c6917e3
Krzysztof Burlinski <krzysztof.burlinski@gmail.com>
2016-09-03 13:23:34 +0200

    Quick fix.

--- hand_evaluator.js
+++ hand_evaluator.js
@@ -7,18 +7,29 @@ function isFigure(card) {
 module.exports = {
 
     eval_hand: function(cards, community) {
-        // check if figure
-        if (isFigure(cards[0]) || isFigure(cards[1]))
-            return 1000000
+        if (cards[0].rank === cards[1].rank) {
+            // Check if pair of figures
+            if (isFigure(cards[0]) return 1000000
+            }
 
-        // check if pair
-        if (cards[0].rank === cards[1].rank)
-            return 1000000
+            // // check if figure
+            // if (isFigure(cards[0]) || isFigure(cards[1]))
+            //     return 700000
+
+            // check if pair
+
+
+        }
         return 0
     },
 
-    eval_hand_with_community: function() {
+    eval_hand_with_3community: function() {
+
         var allCards = _.concat(cards, community);
+        allCards.map()
+            // Count number of pairs.
+
+
         // we expect at least one pair. At this stage.
 
 
