00e192b88256177c8ee31628ad312501dcee5562
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 13:10:14 +0200

    tests, ES6

--- test/test.js
+++ test/test.js
@@ -1,17 +1,20 @@
 var tap = require('tap')
 
+const game1 = require(__dirname + '/../data/game1.json')
 const url = `localhost:1337/action=bet_request&game_state=`
 const options = {
     uri: url,
     // qs: {
     //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
     // },
+    body: game1,
     json: true // Automatically parses the JSON string in the response
 };
-const game1 = require(__dirname + '/../data/game.json')
+
+console.log(game1)
 const rp = require('request-promise')
 
-rp(options).post(url).then(console.log).catch(console.error)
+rp(options).then(console.log).catch(console.error)
 
 
 tap.test('some async stuff', () => {
