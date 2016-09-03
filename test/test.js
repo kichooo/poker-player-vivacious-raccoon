128eb63cd6d8897255c9851c36ca20ce7959eb8b
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 16:32:06 +0200

    test final game

--- test/test.js
+++ test/test.js
@@ -4,20 +4,28 @@ const url = `http://0.0.0.0:1337/`
 const handEval = require('../hand_evaluator')
 const game1 = require(__dirname + '/../data/game1.json')
 
-const options = {
-    uri: url,
-    method: 'POST',
-    body: `action=bet_request&game_state=` + JSON.stringify(game1),
-    // json: true // Automatically parses the JSON string in the response
-};
-
 tap.test('smoke test', () =>
-    rp(options)
-        .then(d => {
-            console.log(d)
-            tap.ok(d)
-        })
-        .catch(console.error)
+    rp({
+        uri: url,
+        method: 'POST',
+        body: `action=bet_request&game_state=` + JSON.stringify(game1),
+        // json: true // Automatically parses the JSON string in the response
+    }).then(d => {
+        console.log(d)
+        tap.ok(d)
+    }).catch(console.error)
+)
+
+tap.test('test end game', () =>
+    rp({
+        uri: url,
+        method: 'POST',
+        body: `action=bet_request&game_state=` + JSON.stringify(game1),
+        // json: true // Automatically parses the JSON string in the response
+    }).then(d => {
+        console.log(d)
+        tap.ok(d)
+    }).catch(console.error)
 )
 
 const cards = [
