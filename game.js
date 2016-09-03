7e0408e7e1a6671fcff0d80918864e2873e6e6dd
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 11:31:42 +0200

    game json

--- game.js
+++ game.js
@@ -1,33 +1,37 @@
-module.exports = {
-    // The small blind in the current round. The big blind is twice
-    // the small blind
-    SmallBlind: int `json:"small_blind"`
+module.exports = function (g) {
 
-    // The amount of the largest current bet from any one player
-    CurrentBuyIn int `json:"current_buy_in"`
+    return {
+        // The small blind in the current round. The big blind is twice
+        // the small blind
+        SmallBlind: parseInt(g.small_blind),
 
-    // The size of the pot (sum of the player bets)
-    Pot int `json:"pot"`
+        // The amount of the largest current bet from any one player
+        CurrentBuyIn: g.current_buy_in,
 
-    // Minimum raise amount. To raise you have to return at least:
-    //     current_buy_in - players[in_action][bet] + minimum_raise
-    MinimumRaise int `json:"minimum_raise"`
+        // The size of the pot (sum of the player bets)
+        Pot: parseInt(g.pot),
 
-    // The index of the player on the dealer button in this round
-    // The first player is (dealer+1)%(players.length)
-    Dealer int `json:"dealer"`
+        // Minimum raise amount. To raise you have to return at least:
+        //     current_buy_in - players[in_action][bet] + minimum_raise
+        MinimumRaise: parseInt(g.minimum_raise),
 
-    // Number of orbits completed. (The number of times the dealer button
-    // returned to the same player.)
-    Orbits int `json:"orbits"`
+        // The index of the player on the dealer button in this round
+        // The first player is (dealer+1)%(players.length)
+        Dealer: parseInt(g.dealer), //int `json:"dealer"`
 
-    // The index of your player, in the players array
-    InAction int `json:"in_action"`
+        // Number of orbits completed. (The number of times the dealer button
+        // returned to the same player.)
+        Orbits: parseInt(g.orbits), // int `json:"orbits"`
 
-    // An array of the players. The order stays the same during the
-    // entire tournament
-    Players []Player `json:"players"`
+        // The index of your player, in the players array
+        InAction: parseInt(g.in_action),  //int `json:"in_action"`
 
-    // Finally the array of community cards.
-    CommunityCards []Card `json:"community_cards"`
+        // An array of the players. The order stays the same during the
+        // entire tournament
+        Players: g.players,// []Player `json:"players"`
+
+        // Finally the array of community cards.
+        CommunityCards: g.community_cards // []Card `json:"community_cards"`
+    }
+
 }
