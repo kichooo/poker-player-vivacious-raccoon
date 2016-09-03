f940f04d72d99060be3b1ee1c5759cf363e05e7b
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 11:17:12 +0200

    game json

--- game.js
+++ game.js
@@ -1,36 +1,37 @@
-module.exports = function (gameIn) {
+module.exports = function (g) {
+
     return {
         // The small blind in the current round. The big blind is twice
         // the small blind
-        SmallBlind: parseInt(gameIn.small_blind),
+        SmallBlind: parseInt(g.small_blind),
 
         // The amount of the largest current bet from any one player
-        CurrentBuyIn: gameIn.CurrentBuyIn,
+        CurrentBuyIn: g.current_buy_in,
 
         // The size of the pot (sum of the player bets)
-        Pot: parseInt(gameIn.pot),
+        Pot: parseInt(g.pot),
 
         // Minimum raise amount. To raise you have to return at least:
         //     current_buy_in - players[in_action][bet] + minimum_raise
-        MinimumRaise: parseInt(gameIn.minimum_raise),
+        MinimumRaise: parseInt(g.minimum_raise),
 
         // The index of the player on the dealer button in this round
         // The first player is (dealer+1)%(players.length)
-        Dealer: int `json:"dealer"`
+        Dealer: parseInt(g.dealer), //int `json:"dealer"`
 
         // Number of orbits completed. (The number of times the dealer button
         // returned to the same player.)
-        Orbits int `json:"orbits"`
+        Orbits: parseInt(g.orbits), // int `json:"orbits"`
 
         // The index of your player, in the players array
-        InAction int `json:"in_action"`
+        InAction: parseInt(g.in_action),  //int `json:"in_action"`
 
         // An array of the players. The order stays the same during the
         // entire tournament
-        Players []Player `json:"players"`
+        Players: g.players,// []Player `json:"players"`
 
         // Finally the array of community cards.
-        CommunityCards []Card `json:"community_cards"`
+        CommunityCards: g.community_cards // []Card `json:"community_cards"`
     }
 
 }
