2da67511f13c8dc399b130bf840e5b852247f481
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 13:22:55 +0200

    tests, npm i --save-dev , nodemon -x tap test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,21 @@
-var tap = require('tap')
+const tap = require('tap')
+const rp = require('request-promise')
+const url = `http://0.0.0.0:1337/`
 
 const game1 = require(__dirname + '/../data/game1.json')
-const url = `localhost:1337/action=bet_request&game_state=`
+
 const options = {
     uri: url,
-    // qs: {
-    //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
-    // },
-    body: game1,
-    json: true // Automatically parses the JSON string in the response
+    method: 'POST',
+    body: `action=bet_request&game_state=` + JSON.stringify(game1),
+    // json: true // Automatically parses the JSON string in the response
 };
 
-console.log(game1)
-const rp = require('request-promise')
-
-rp(options).then(console.log).catch(console.error)
-
-
-tap.test('some async stuff', () => {
-    tap.pass('this is fine')
-})
+tap.test('smoke test', () =>
+    rp(options)
+        .then(d => {
+            console.log(d)
+            tap.ok(d)
+        })
+        .catch(console.error)
+)
